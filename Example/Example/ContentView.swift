8cafc6618aaaabdb69185c6d80dfd84a870312fd
vo1x <ak2061@icloud.com>
2026-04-24 10:42:28 +0545

    init: Example project

---
+++ Example/Example/ContentView.swift
@@ -0,0 +1,9 @@
+import FloatingPermissions
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Example App")
+            .frame(minWidth: 320, minHeight: 180)
+    }
+}
