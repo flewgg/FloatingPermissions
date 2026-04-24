c64f9accf798e038f5dddb3d461ed94908d8c2f6
vo1x <ak2061@icloud.com>
2026-04-24 11:09:49 +0545

    Add floating permissions tests

--- Tests/FloatingPermissionsTests/FloatingPermissionsTests.swift
+++ Tests/FloatingPermissionsTests/FloatingPermissionsTests.swift
@@ -1,6 +1,36 @@
+import Foundation
 import Testing
 @testable import FloatingPermissions
 
 @Test func moduleNameIsStable() {
     #expect(String(describing: FloatingPermissions.self) == "FloatingPermissions")
+}
+
+@Test
+func paneURLsUseSecuritySettingsDeepLink() {
+    #expect(
+        FloatingPermissionPane.fullDiskAccess.settingsURL.absoluteString ==
+        "x-apple.systempreferences:com.apple.settings.PrivacySecurity.extension?Privacy_AllFiles"
+    )
+}
+
+@Test
+func typedDisplaysAnchorBuildsDeepLink() {
+    #expect(
+        SystemSettingsDestination.displays(anchor: .resolutionSection).url.absoluteString ==
+        "x-apple.systempreferences:com.apple.Displays-Settings.extension?resolutionSection"
+    )
+}
+
+@Test
+@MainActor
+func controllerAcceptsOnlyUniqueAppBundles() {
+    let controller = FloatingPermissionsController()
+    let appURL = URL(fileURLWithPath: "/Applications/Test.app")
+
+    controller.registerDroppedApp(appURL)
+    controller.registerDroppedApp(appURL)
+    controller.registerDroppedApp(URL(fileURLWithPath: "/tmp/not-an-app.txt"))
+
+    #expect(controller.droppedApps == [appURL])
 }
